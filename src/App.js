55bbebd30e185e42efccd1f2410cb27258b192ee
Caleb Larsen <larsenc91@gmail.com>
2019-02-13 18:36:57 -0600

    Minor initial updates.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className={`${this.props.className} App`}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header>
+          <img src={logo} className="logo" alt="logo" />
           <p>Welcome to caleb-larsen.com. I've only just begun my migration to React, so there's nothing here at the moment. Check back again soon!</p>
         </header>
       </div>
@@ -18,13 +18,8 @@ class App extends Component {
 export default styled(App)`
   text-align: center;
 
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-    height: 40vmin;
-    pointer-events: none;
-  }
-
-  .App-header {
+  header {
+    padding: 0 30px;
     background-color: #282c34;
     min-height: 100vh;
     display: flex;
@@ -35,8 +30,9 @@ export default styled(App)`
     color: white;
   }
 
-  .App-link {
-    color: #61dafb;
+  img.logo {
+    animation: App-logo-spin infinite 20s linear;
+    height: 40vmin;
   }
 
   @keyframes App-logo-spin {
