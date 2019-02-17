a1c4c6604500b0290f1e76689780d88b5910d1a2
Caleb Larsen <larsenc91@gmail.com>
2019-02-16 22:44:15 -0600

    Update home page text regarding theme editor.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export class Home extends PureComponent {
     return (
         <div>
             <img src={logo} className="logo" alt="logo" />
-            <p>Welcome to caleb-larsen.com. I've only just begun my migration to React, so there's not much here at the moment. Check back again soon!</p>
+            <p>Welcome to caleb-larsen.com. I've only just begun my migration to React, so there's not much here at the moment (except for this amazing theme editor below). Check back again soon!</p>
         </div>
     );
   }
