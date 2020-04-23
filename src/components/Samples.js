95f61fc6ca46b02d873efa9504349a07c23aed68
Caleb Larsen <caleb.larsen@rentping.com>
2020-04-22 22:39:10 -0500

    Initial commit with svelte template.

--- src/components/Samples.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from 'react';
-
-export class Samples extends PureComponent {
-  render() {
-    return (
-      <div>I'll eventually have some sample stuff here.</div>
-    );
-  }
-}
--- src/constants/themes.js
+++
@@ -1,8 +0,0 @@
-export const lightTheme = {
-    backgroundColor: '#eeeeee',
-    primary: '#11073b',
-    alternatePrimary: '#877cb0',
-    roundedBorders: true,
-};
-
-export const defaultTheme = lightTheme;
