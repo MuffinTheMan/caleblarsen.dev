75929af667e96f0cee1c05171c5b9e39ec33127b
Caleb Larsen <caleb.larsen@rentping.com>
2020-04-26 23:01:26 -0500

    Change favicon and gut bloated/unused CSS, etc.

--- src/App.svelte
+++ src/App.svelte
@@ -2,25 +2,3 @@
 	<h1>Howdy.</h1>
 	<p>So I decided to toss my site and rewrite it with <a href="https://svelte.dev/" target="_blank" rel="nofollow">Svelte</a>. The good thing is that my site wasn't much to look at before anyway, so let's see how this goes! Source code for this site is on <a href="https://github.com/MuffinTheMan/caleblarsen.dev" target="_blank" rel="nofollow">GitHub</a>.</p>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
